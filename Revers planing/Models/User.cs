8d838c928f10a22d01ecbad0b62e5f21a40e0a57
Ren Williams <vadkakr@gmail.com>
2025-12-02 18:13:07 +0700

    1

--- Revers planing/Models/User.cs
+++ Revers planing/Models/User.cs
@@ -6,10 +6,4 @@ public class User
     public string Name { get; set; } = String.Empty;
     public string Email { get; set; } = String.Empty;
     public string PasswordHash { get; set; } = String.Empty;
-
-    void login(string email, string password)
-    {
-    }
-
-    string getFullName() => Name; // ?Зачем нам этот метод, если ы и так можем получить имя через гет
 }
